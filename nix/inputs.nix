0c32b1556798e832ea41315f201c0029381cfd5e
mavnn <michael@mavnn.co.uk>
2019-08-30 17:24:25 +0100

    Working ghc compiles, added formatter

--- nix/inputs.nix
+++ nix/inputs.nix
@@ -4,7 +4,17 @@ let
   stdenv = pkgs.stdenv;
 
   shakePkgs = pkgs.haskellPackages.ghcWithPackages (ps: with ps; [ghc shake]);
+
+  ormoluSrc = pkgs.fetchFromGitHub {
+    owner  = "tweag";
+    repo   = "ormolu";
+    rev    = "d61b9101ca7e1bd9edbc7767eae69e9c7732a0a8";
+    sha256 = "1hkrwm1bsriw6pwvml4wlbpfnsvciipdlp2qy8xmmb1wbx0ssiy5";
+  };
+  ormolu = import ormoluSrc;
 in
 [
   shakePkgs
+  pkgs.gcc
+  ormolu.ormolu
 ]
